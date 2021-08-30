d4af0968820267dead3211d41146d89c5f9d9139
Andrej Čermák <andrej.cermak.1999@gmail.com>
2021-08-30 12:37:27 +0200

    Adding default values

--- bioconductor/variables.tf
+++ bioconductor/variables.tf
@@ -1,25 +1,31 @@
 variable "instance_name" {
   type = string
+  default = "default"
 }
 
 variable "ssh" {
   type = string
+  default = "default"
 }
 
 variable "local_network_id" {
   type = string
+  default = "default"
 }
 
 variable "user_name" {
   type = string
+  default = "default"
 }
 
 variable "user_email" {
   type = string
+  default = "default"
 }
 
 variable "floating_ip" {
   type = string
+  default = "default"
 }
 
 variable "token" {
